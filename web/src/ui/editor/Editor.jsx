e5dd7ca6cff78d7672acf66eda6b5ad3c737cd7b
Stefan Werner <stefan.werner@infai.org>
2023-11-25 00:11:03 +0100

    fix it

--- web/src/ui/editor/Editor.jsx
+++ web/src/ui/editor/Editor.jsx
@@ -307,7 +307,7 @@ export const Editor = () => {
         title: 'hyperonym',
       },
       {
-        title: 'atonyms',
+        title: 'antonyms',
       },
       {
         title: '(anti-/syn)-thesis',
